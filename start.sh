7eb0fa8f4723b3bdaa874b8fc5c55a6014430366
Hitesh Bansal <54891244+ihacker42@users.noreply.github.com>
2024-06-05 21:28:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
+# Find the correct PHP version installed
+PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;")
+
 # Start PHP-FPM
-service php7.4-fpm start || service php8.1-fpm start || service php-fpm start
+service php${PHP_VERSION}-fpm start
 
 # Start Nginx
 exec nginx -g 'daemon off;'
